2c8f8eb016142b2b30dbf4fe1052ba8acdad3ea1
kasper <kasper@pop-os.localdomain>
2021-11-20 18:54:41 +0200

    Add files

--- index.ts
+++ index.ts
@@ -49,6 +49,11 @@ const print_all_files = () => {
   console.log(added)
 }
 
+const LightenDarkenColor = (col, amt) => {
+  col = parseInt(col, 16);
+  return (((col & 0x0000FF) + amt) | ((((col >> 8) & 0x00FF) + amt) << 8) | (((col >> 16) + amt) << 16)).toString(16);
+}
+
 const createCard = (x,y, title) => {
   const data = {
     data:{
@@ -58,7 +63,7 @@ const createCard = (x,y, title) => {
     },
     metadata: `${title}123123`,
     style: {
-          cardTheme: "#2d9bf0"
+          cardTheme: "#FF3B10"
      },
      geometry: {
           x: x,
@@ -71,19 +76,39 @@ const createCard = (x,y, title) => {
   return data
 }
 
-const createNewCardsFromFiles = async () => {
-  // TEST
-  const modified = await core.getInput('modified_files')
+const getAllFiles= () => {
+  const modified = core.getInput('modified_files')
+  const added = core.getInput('added_files')
+  return modified.concat(added)
+}
+
+const createNewCardsFromAdded = async () => {
+  const modified = await core.getInput('added_files')
   .split(" ")
   .map((title, index) => createCard(0,index*100,title))
   return modified
- 
 }
 
+const updateCards = async () => {
+  const exsistingItems = await getCards(`${core.getInput('board_id')}`)
+
+  const titlesAndId = exsistingItems.data.map(x => {
+    return {
+      title: x.data.title,
+      id: x.id
+    }
+  })
+
+  const allFiles = getAllFiles()
+
+  const [exsistingFiles, newFiles] = titlesAndId.reduce(([p, f], e) => (allFiles.contains(e.title)? [[...p, e], f] : [p, [...f, e]]), [[], []]);
+  console.log(exsistingFiles)
+  console.log(newFiles)
+}
 
 const createCards = async (boardId) => {
   const requestUrl = `https://api.miro.com/v2/boards/${boardId}/cards`
-  const data = await createNewCardsFromFiles()
+  const data = await createNewCardsFromAdded()
   console.log(data)
   const result = await Promise.all( data.map(async (x) => {
     const res = await post(requestUrl, x);
@@ -111,10 +136,37 @@ const post = async (url, data) => {
   return response.json()
 }
 
+const patch= async (url, data) => {
+  const response = await fetch(url, {
+    method: 'PATCH',
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${core.getInput('secret_key')}`
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data),
+  });
+
+  return response.json()
+}
+
 const getBoardContents = async (boardId) => {
   const requestUrl = `https://api.miro.com/v2/boards/${boardId}/widgets`
   const result = await get(requestUrl)
   console.log(result)
+  return result
+}
+
+const getCards = async (boardId) => {
+  const requestUrl = `https://api.miro.com/v2/boards/${boardId}/widgets&type=card`
+  const result = await get(requestUrl)
+  console.log(result)
+  return result
 }
 
 const get = async (url) => {
@@ -137,3 +189,4 @@ const get = async (url) => {
 print_all_files()
 
 createCards(`${core.getInput('board_id')}`)
+updateCards()
